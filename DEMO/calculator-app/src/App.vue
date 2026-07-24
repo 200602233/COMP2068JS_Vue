51b555b5eb8e3906b225f5be16b999eb7271910d
200602233 <200602233@student.georgianc.on.ca>
2026-07-23 20:56:34 -0400

    created the start for script following the Vue links listed in code. - imported reference to create reactive variables with vue - created the variables - created empty functions for the calculator +,-,*,/ and clear
    Template:
    - added comments on how to start and complete code, as well as links for reference.

--- DEMO/calculator-app/src/App.vue
+++ DEMO/calculator-app/src/App.vue
@@ -1,4 +1,65 @@
-<script>
-// code for calculator goes here
+<!-- 
+ Resources:
+    Setup:
+        * https://vuejs.org/guide/quick-start.html
+    
+    Code: 
+        * https://vuejs.org/guide/essentials/reactivity-fundamentals.html 
+        * https://vuejs.org/guide/essentials/event-handling.html
+        * https://vuejs.org/guide/essentials/template-syntax.html
+        * 
 
+
+ -->
+
+
+<script setup>
+// https://vuejs.org/guide/essentials/reactivity-fundamentals.html 
+//imports ref to creaet reactive vars
+import { ref } from 'vue'
+
+//vars
+const num1 = ref(0)
+const num2 = ref(0)
+const results = ref(0)
+
+
+// https://vuejs.org/guide/essentials/event-handling.html
+// fucntions
+function add(0){
+
+}
+
+function minus(0){
+    
+}
+
+function multiply(0){
+    
+}
+
+function divide(0){
+    
+}
+
+function clear(0){
+    
+}
 </script>
+
+
+<template>
+    <!-- https://vuejs.org/guide/essentials/template-syntax.html -->
+    <!-- add layout here, div, h1, inputs,etc.. -->
+     <!-- then connect to above functions -->
+      <!-- add class or id's for easier nav (also ref for style.css) -->
+    <div>
+        <!-- https://vuejs.org/guide/essentials/forms.html -->
+         <!-- inputs -->
+
+        
+         <!-- https://vuejs.org/guide/essentials/event-handling.html -->
+        <!-- @click -->
+    </div>
+
+</template>

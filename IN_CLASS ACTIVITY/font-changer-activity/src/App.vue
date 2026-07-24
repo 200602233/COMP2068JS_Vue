267cbfae695b4669c72ff7ca42453aa1d4fe71fd
200602233 <200602233@student.georgianc.on.ca>
2026-07-24 14:39:22 -0400

    completed script for App.vue: - imported ref from vue - declared fontSize variable - created both functions increase and decrease

--- IN_CLASS ACTIVITY/font-changer-activity/src/App.vue
+++ IN_CLASS ACTIVITY/font-changer-activity/src/App.vue
@@ -1,4 +1,26 @@
-<script setup></script>
+<script setup>
+// import ref creates reactive variables
+// a reactive variable is one that Vue can track for changes, so it can update the DOM accordingly
+// https://vuejs.org/guide/essentials/reactivity-fundamentals.html 
+import { ref } from 'vue'
+
+// fontsize variable
+// we pass in a font size in px to our reactive variable
+// and assign is to a const value
+// fontSize is initialized at 40px
+const fontSize = ref(40) //pxs
+
+// function definitions to increase or decrease the value of our fontSize variable
+// https://vuejs.org/guide/essentials/event-handling.html
+function increase(){
+  fontSize.value++
+}
+function decrease(){
+  fontSize.value--
+}
+
+// end of script setup
+</script>
 
 <template>
   <h1>You did it!</h1>

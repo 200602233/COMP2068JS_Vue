0cb8265d3d23a5ec82db34076bfcddb98c9da175
200602233 <200602233@student.georgianc.on.ca>
2026-07-23 21:11:56 -0400

    commented out import that was added during preset adding, now app runs

--- DEMO/calculator-app/src/main.js
+++ DEMO/calculator-app/src/main.js
@@ -1,7 +1,7 @@
 // main.js is the entry point of VUE application.
 // create the vue app and adds our root component (App.vue)
 
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
